a58acf16165e64abcb1248644abc2e1490091f5d
regadas <filipe.regadas@outplay.com>
2015-03-19 14:25:05 +0000

    First commit

---
+++ docker/recipes/install.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: docker
+# Recipe:: install
+#
+
+case node[:platform]
+when "ubuntu","debian"
+  package "docker.io" do
+    action :install
+  end
+when 'centos','redhat','fedora','amazon'
+  package "docker" do
+    action :install
+  end
+end
+
+service "docker" do
+  action :start
+end
